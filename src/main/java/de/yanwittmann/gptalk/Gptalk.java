fdcb74f4e8104dd71f22485a4791ff8efe259e48
Yan Wittmann <order@yanwittmann.de>
2023-02-04 18:56:36 +0100

    Initial project files
    Signed-off-by: Yan Wittmann <order@yanwittmann.de>

---
+++ src/main/java/de/yanwittmann/gptalk/Gptalk.java
@@ -0,0 +1,10 @@
+package de.yanwittmann.gptalk;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Gptalk implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/de/yanwittmann/gptalk/client/GptalkClient.java
@@ -0,0 +1,13 @@
+package de.yanwittmann.gptalk.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class GptalkClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
